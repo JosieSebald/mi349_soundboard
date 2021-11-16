0816df72a92f0ec7c535c38012ae7ec2e156d9c9
Josie Sebald <sebaldjo@msu.edu>
2021-11-16 17:37:52 -0500

    changed console text in js

--- soundboard.js
+++ soundboard.js
@@ -1,21 +1,21 @@
 var audioCrash = document.getElementById("crash")
 var buttonElement = document.getElementById('Crash-Button')
 buttonElement.addEventListener('click', function () {
-    console.log('I was clicked!')
+    console.log('Crash!')
     audioCrash.play()
   })
 
 var audioMoo = document.getElementById("moo")
 var buttonElement = document.getElementById('Cow-Button')
 buttonElement.addEventListener('click', function () {
-    console.log('I was clicked!')
+    console.log('Moo!')
     audioMoo.play()
   })
 
   var audioScream = document.getElementById("scream")
   var buttonElement = document.getElementById('Scream-Button')
   buttonElement.addEventListener('click', function () {
-      console.log('I was clicked!')
+      console.log('Aaah!')
       audioScream.play()
     })
 
